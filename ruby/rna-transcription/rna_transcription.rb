06a23d2b8cf4787a4750b9cc99dc5ebb6badd4e0
chanjman <mariozugaj@gmail.com>
2017-01-02 21:27:04 +0100

    Add rna_transcription.rb

---
+++ ruby/rna-transcription/rna_transcription.rb
@@ -0,0 +1,10 @@
+# Given a DNA strand, returns its RNA complement (per RNA transcription)
+class Complement
+  def self.of_dna(dna)
+    dna.match =~ /[^GCTA]/ ? '' : dna.tr('GCTA', 'CGAU')
+  end
+end
+
+module BookKeeping
+  VERSION = 4
+end
